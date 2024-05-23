9d1e8d763395f04faa462a271fe1f6ffe66b49e5
mauricio.quezada <mauricio.quezada@sma.gob.cl>
2024-05-23 10:00:57 -0400

    chore: Update makefile with new targets for test updates

---
+++ lambda_function.py
@@ -0,0 +1,27 @@
+from function import EmailManager
+from function import NotionManager
+
+
+def lambda_handler(event, context):
+    data = EmailManager.get_payment_email()
+
+    db_name = 'Saldos mensuales'
+    database_id = NotionManager.get_id_data_bases(db_name)
+    month_id = NotionManager.get_last_month_id(database_id)
+    for d in data:
+        print(f"De: {d['from']}")
+        print(f"Asunto: {d['subject']}")
+        print(f"Monto: {d['monto']}")
+        print(f"Fecha: {d['fecha']} {d['hora']}")
+        print(f"Comercio: {d['comercio']}")
+        print('----------')
+
+        if 'USD' not in d['monto']:
+            amount = int(d['monto'].replace('$', '').replace('.', '').replace(',', '.'))
+            data = {
+                'name': d['comercio'],
+                'amount': amount,
+            }
+            result = NotionManager.insert_payment_data_by_month(data, month_id)
+
+        print(result)
--- makefile
+++ makefile
@@ -9,3 +9,25 @@ cleanimports:
 	autoflake -r -i --remove-all-unused-imports --ignore-init-module-imports .
 
 clean-lint: cleanimports black lint
+
+update-test:
+	zip -r function.zip .
+	aws lambda update-function-code --function-name notify-purchases --zip-file fileb://function.zip
+	aws lambda get-function --function-name notify-purchases
+	rm function.zip
+
+update-test2:
+	mkdir package
+	pip install -r requirements.txt -t package
+	cd package
+	zip -r ../function.zip .
+	cd ..
+	cd data
+	zip -r ../function.zip ./data
+	cd ..
+	cd function
+	zip -r ../function.zip ./function
+	cd ..
+	zip function.zip lambda_function.py
+	aws lambda update-function-code --function-name notify-purchases --zip-file fileb://function.zip
+	rm -rf packagez
