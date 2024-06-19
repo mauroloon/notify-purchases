2285c617e6ee40817c7d6d0ab03ec29e7400ee68
mauricio.quezada <mauricio.quezada@sma.gob.cl>
2024-06-19 09:08:35 -0400

    feat: Update token retrieval and storage in TokenManager

--- utils/token.py
+++ utils/token.py
@@ -9,20 +9,12 @@ from function.DynamoManager import DynamoManager
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
-
-# Obtener la ruta absoluta del directorio actual
 current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Construir las rutas a los archivos credentials.json y token.json
 credentials_file_path = os.path.join(current_directory, '..', 'tmp/data', 'credentials.json')
-token_file_path = os.path.join(current_directory, '..', 'tmp/data', 'token.json')
 
 
 def get_credentials():
     credentials = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
 
     token_data = DynamoManager.get_gmail_data()
 
@@ -31,19 +23,12 @@ def get_credentials():
             token = json.loads(token_data['data'])
         credentials = Credentials.from_authorized_user_info(token, SCOPES)
 
-    """ if os.path.exists(token_file_path):
-        credentials = Credentials.from_authorized_user_file(token_file_path, SCOPES) """
-
-    # If there are no (valid) credentials available, let the user log in.
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(credentials_file_path, SCOPES)
             credentials = flow.run_local_server(port=0)
-        """ # Save the credentials for the next run
-        with open(token_file_path, 'w') as token:
-            token.write(credentials.to_json()) """
 
         DynamoManager.upload_gmail_data(credentials.to_json())
 
