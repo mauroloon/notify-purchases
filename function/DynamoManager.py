3e4ea0cf5059ea553d3f0d41a72e1bdd0ba3ee7e
mauricio.quezada <mauricio.quezada@sma.gob.cl>
2024-06-25 08:13:14 -0400

    feat: Clean Gmails table in DynamoDB

--- function/DynamoManager.py
+++ function/DynamoManager.py
@@ -50,3 +50,20 @@ class DynamoManager:
         items = response['Items']
         last_item = items[-1] if items else None
         return last_item
+
+    @staticmethod
+    def clean_gmail_data() -> None:
+        """
+        Limpia la tabla Gmails.
+
+        Returns:
+            - None
+        """
+        dynamodb = boto3.resource('dynamodb', region_name=REGION_AWS)
+        table = dynamodb.Table('Gmails')
+
+        response = table.scan()
+        items = response['Items']
+        for item in items:
+            table.delete_item(Key={'id': item['id']})
+        return
--- utils/token.py
+++ utils/token.py
@@ -6,11 +6,15 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 from function.DynamoManager import DynamoManager
+from utils import Logger
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 credentials_file_path = os.path.join(current_directory, '..', 'tmp/data', 'credentials.json')
+
+
+logger = Logger(__name__)
 
 
 def get_credentials():
@@ -25,7 +29,11 @@ def get_credentials():
 
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
+            try:
+                credentials.refresh(Request())
+            except Exception as e:
+                logger.error(f'Error al refrescar las credenciales: {e}')
+                DynamoManager.clean_gmail_data()
         else:
             flow = InstalledAppFlow.from_client_secrets_file(credentials_file_path, SCOPES)
             credentials = flow.run_local_server(port=0)
